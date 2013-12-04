cc17b62a515025661b7d04d54a2b157a73931497
Zlatko Đurić <zladuric@gmail.com>
2013-12-04 14:10:08 +0100

    Jade text, tags and attributes example

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
-var app = require('express')().set('views', __dirname + '/views').set('view engine', 'jade')
-    ;
+var app = require('express')().set('views', __dirname + '/views').set('view engine', 'jade');
+app.use(require('express').static(__dirname + '/public'));
 app.locals.pretty = true;
 app.get('/', function(req, res) {
-    res.render('index', {title: 'So let\'s look at jade.'});
+    res.render('index', {title: 'So let\'s look at jade.', someVariable: 'Template variable'});
 });
 require('http').createServer(app).listen(3000);
