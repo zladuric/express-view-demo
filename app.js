f34543de89c520c71fa9184250c5613968c25991
Zlatko Đurić <zladuric@gmail.com>
2013-12-04 13:52:14 +0100

    Trying the same with Handlebars, with partials included.

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
-var app = require('express')().set('view engine', 'jade');
-app.locals.pretty = true;
+var app = require('express')().set('views', __dirname + '/views').set('view engine', 'hbs')
+    , hbs = require('hbs')
+    ;
+hbs.registerPartial('part', require('fs').readFileSync('views/partial.hbs', 'utf-8'));
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Hello from Jade'});
+    res.render('index', {title: 'Would you stop it with saluting?'});
 });
 require('http').createServer(app).listen(3000);
