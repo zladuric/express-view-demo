f212e2073aac4546b23fcac1354e508a8b8409c1
Zlatko Đurić <zladuric@gmail.com>
2013-12-04 13:15:00 +0100

    Switched to Jade

--- app.js
+++ app.js
@@ -1,5 +1,5 @@
-var app = require('express')().set('view engine', 'ejs');
+var app = require('express')().set('view engine', 'jade');
 app.get('/', function(req, res) {
-    res.render('index', {title: 'Hello from EJS'});
+    res.render('index', {title: 'Hello from Jade'});
 });
 require('http').createServer(app).listen(3000);
