9bed83f99f59afa6938e60ce35a3995935457229
Zlatko Đurić <zladuric@gmail.com>
2013-12-04 13:08:37 +0100

    Switched to Rails-erb-like ejs engine

--- app.js
+++ app.js
@@ -1,9 +1,5 @@
 var app = require('express')().set('view engine', 'ejs');
-var options = {
-    title: 'Hello world'
-    , name: 'Hello Javascript meetup'
-}
 app.get('/', function(req, res) {
-    res.render('index', {title: options.title});
+    res.render('index', {title: 'Hello from EJS'});
 });
 require('http').createServer(app).listen(3000);
